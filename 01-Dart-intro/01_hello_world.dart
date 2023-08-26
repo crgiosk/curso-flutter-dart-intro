e6121d2d459318ae4a1b42dbee7534bb935d108a
sergio.noriega <crgiosk.23@gmail.com>
2023-08-26 15:11:07 -0500

    creando primero hola mundo y definicion de 'final y const'

---
+++ 01-Dart-intro/01_hello_world.dart
@@ -0,0 +1,21 @@
+void main() {
+  
+  //var name = 'Sergio';
+  String name = 'Sergio';
+  
+  // final name = 'Sergio'
+  // final una vez se le da valor no se puede modificar
+  
+  //late final name; la misma cosa, pero el valor 
+  //es que se inicia a algun momento
+  
+  //const name
+  //una constante, se asgina a nivel de compilacion (construccion)
+  //y el final a nivel de ejecucion
+  
+  print('My name is $name');
+  print('My name is ${name.toUpperCase()}');
+  
+
+
+}
