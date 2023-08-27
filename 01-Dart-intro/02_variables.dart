8d59070c1885a96174865c692cee3b6e9dd482ea
sergio.noriega <crgiosk.23@gmail.com>
2023-08-27 11:05:32 -0500

    02 - Tipos de variables. (Dynamic)

--- 01-Dart-intro/02_variables.dart
+++ 01-Dart-intro/02_variables.dart
@@ -5,8 +5,17 @@ void main() {
   final List<String> abilities = ['impostor'];
   //final sprites = ['impostor'];
   final sprites = <String>['ditto/front.png', 'ditto/back.png'];
-
-
+  //dynamic es un tipo de variable que puede tomar cualquier valor, muy parecido
+  // al any en kotlin
+  dynamic errorMessage = "Message";
+  errorMessage = true;
+  //lista
+  errorMessage = [1,2,3,4,5];
+  //set de datos
+  errorMessage = {1,2,3,4,5};
+  //una funcion que retorna un true (parecido al lamda?)
+  errorMessage = () => true;
+  errorMessage = null;
 
   //con 3 lineas, se le dice a dart, que se va a hacer un String multilinea
   print("""
@@ -16,6 +25,7 @@ void main() {
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   
   """
   );
