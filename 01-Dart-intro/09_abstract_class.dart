213027b542413a8a10b9ea1ec5de74894222a5dd
sergio.noriega <crgiosk.23@gmail.com>
2023-08-31 10:01:43 -0500

    09 - extends - abstract class

--- 01-Dart-intro/09_abstract_class.dart
+++ 01-Dart-intro/09_abstract_class.dart
@@ -1,5 +1,21 @@
 void main() {
 
+  final windPlante = WindPlant(initialEnergdy: 100);
+
+
+  print("windLeft = ${windPlante.energyLeft}");
+  print("charging phone...");
+  print("windLeft = ${chargePhone(windPlante)}");
+
+
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft <10 ) {
+    throw Exception("Sin suficiente energia");
+  }
+
+  return plant.energyLeft - 10;
 }
 
 enum PlantType {
@@ -16,7 +32,19 @@ abstract class EnergyPlant {
     required this.energyLeft,
     required this.type
   }) : assert(energyLeft > 0, "energyLeft msut be >0"),
-       assert(type != PlantType.none, "type must be not none");
+        assert(type != PlantType.none, "type must be not none");
 
   void consumeEnergy( double amountEnergy);
+}
+
+class WindPlant extends EnergyPlant {
+
+  WindPlant({required double initialEnergdy})
+      : super(energyLeft: initialEnergdy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amountEnergy) {
+    energyLeft -= amountEnergy;
+
+  }
 }
